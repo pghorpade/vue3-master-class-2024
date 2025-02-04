5ac9407fff13764a02074e4521986f5bdb4984a6
pghorpade <pghorpade@library.ucla.edu>
2025-02-03 16:48:44 -0800

    feat: add auto routes and file based routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/project',
-      name: 'project',
-      component: () => import('@/views/ProjectView.vue'),
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue'),
-    },
-    {
-      // /project/1 or /project/2
-      path: '/project/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color:red;' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router
